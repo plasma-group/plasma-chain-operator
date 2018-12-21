15b945d9902308d2e1c0fbd2da80dfc3537a24bd
Karl Floersch <karl@karlfloersch.com>
2018-12-21 12:46:48 -0500

    Add example leveldb & acct/tx generation

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('hello world')
+const levelup = require('levelup')
+const leveldown = require('leveldown')
+
+// 1) Create our store
+const db = levelup(leveldown('./db'))
+
+// 2) Put a key & value
+db.put('name', 'levelup', function (err) {
+  if (err) return console.log('Ooops!', err) // some kind of I/O error
+
+  // 3) Fetch by key
+  db.get('name', function (err, value) {
+    if (err) return console.log('Ooops!', err) // likely the key was not found
+
+    // Ta da!
+    console.log('name=' + value)
+  })
+})
--- mock-transactions.js
+++ mock-transactions.js
@@ -1,16 +1,33 @@
+const Web3 = require('web3')
+const web3 = new Web3('ws://localhost:8546')
+const BigNumber = require('bignumber.js')
+
+// Generate a bunch of private keys
+const privateKeys = []
+for (let i = 0; i < 100; i++) {
+  privateKeys.push(web3.utils.sha3(i.toString()))
+}
+// Generate a bunch of accounts
+const accounts = []
+for (const privateKey of privateKeys) {
+  accounts.push(web3.eth.accounts.privateKeyToAccount(privateKey))
+}
+console.log(accounts)
+
+// Generate a sample transaction
 const sampleTr = {
-  sender: 0,
-  recipient: 0,
-  type: 0,
-  start: 0,
-  offset: 0,
-  block: 0
+  sender: accounts[0].address,
+  recipient: accounts[1].address,
+  type: BigNumber('0'),
+  start: BigNumber('10'),
+  offset: BigNumber('1'),
+  block: BigNumber('0')
 }
 
 const sig = {
-  v: 0,
-  r: 0,
-  s: 0
+  v: BigNumber('0'),
+  r: BigNumber('0'),
+  s: BigNumber('0')
 }
 
 const tx = {
