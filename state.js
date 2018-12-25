26ed10c418b60df2471becb290f98fec234819fe
Karl Floersch <karl@karlfloersch.com>
2018-12-25 13:37:01 -0500

    Add block changing logic

--- state.js
+++ state.js
@@ -1,8 +1,11 @@
+const fs = require('fs-extra')
 const BN = require('./eth.js').utils.BN
 
 const START_BYTE_SIZE = require('./constants.js').START_BYTE_SIZE
 const TYPE_BYTE_SIZE = require('./constants.js').TYPE_BYTE_SIZE
 const BLOCKNUMBER_BYTE_SIZE = require('./constants.js').BLOCKNUMBER_BYTE_SIZE
+
+const timeout = ms => new Promise(resolve => setTimeout(resolve, ms))
 
 function getAddressToTokenKey (address, type, start) {
   const buffers = [address,
@@ -25,8 +28,10 @@ function getTotalDepositsKey (type) {
 }
 
 class State {
-  constructor (db, callback) {
+  constructor (db, txLogDirectory) {
     this.db = db
+    this.txLogDirectory = txLogDirectory
+    this.tmpTxLogFile = this.txLogDirectory + 'tmp-tx-log.bin'
     this.lock = {}
   }
 
@@ -40,16 +45,37 @@ class State {
       if (err.notFound) {
         console.log('No blocknumber found! Starting from block 0.')
         this.blocknumber = new BN(0)
-        await this.db.put('blocknumber', this.blocknumber.toArrayLike(Buffer, 'big', BLOCKNUMBER_BYTE_SIZE))
+        await this.db.put(Buffer.from('blocknumber'), this.blocknumber.toArrayLike(Buffer, 'big', BLOCKNUMBER_BYTE_SIZE))
       } else { throw err }
     }
+    // Open a write stream for our tx log
+    this.writeStream = fs.createWriteStream(this.tmpTxLogFile, { flags: 'a' })
+  }
+
+  async startNewBlock () {
+    // Start a global lock as we increment the block number. Note that we will have to wait until all other locks are released
+    this.lock.all = true
+    // Wait until all other locks are released
+    while (Object.keys(this.lock).length !== 1) {
+      console.log('Waiting to release global lock')
+      await timeout(Math.random() * 10 + 2)
+    }
+    // Everything should be locked now that we have a `lock.all` activated. Time to increment the blocknumber
+    this.blocknumber = this.blocknumber.add(new BN(1))
+    await this.db.put(Buffer.from('blocknumber'), this.blocknumber.toArrayLike(Buffer, 'big', BLOCKNUMBER_BYTE_SIZE))
+    // Start a new tx log
+    this.writeStream.end()
+    await fs.rename(this.tmpTxLogFile, this.txLogDirectory + +new Date())
+    // Release our lock
+    console.log('Releasing global lock')
+    delete this.lock.all
   }
 
   async addDeposit (recipient, type, amount) {
     // Check if there is a lock on this token type or recipient
-    if (this.lock.hasOwnProperty(recipient) || this.lock.hasOwnProperty(type)) {
-      setTimeout(this.addDeposit(recipient, type, amount), Math.random() * 10 + 1)
-      return
+    while (this.lock.hasOwnProperty('all') || this.lock.hasOwnProperty(recipient) || this.lock.hasOwnProperty(type)) {
+      console.log('Locked! Waiting to release')
+      await timeout(Math.random() * 10 + 2)
     }
     // Create a lock on the account and token type
     this.lock[recipient] = true
@@ -67,9 +93,9 @@ class State {
     }
     console.log('Total deposits:', totalDeposits)
     // Put the updated totalDeposits and owned token ranges
+    const newTotalDeposits = totalDeposits.add(amount)
     try {
       // Put the new owned token range and the new total deposits
-      const newTotalDeposits = totalDeposits.add(amount)
       const ops = [
         { type: 'put', key: getAddressToTokenKey(recipient, type, totalDeposits), value: Buffer.from([1]) },
         { type: 'put', key: getTokenToTxKey(type, totalDeposits), value: this.blocknumber.toArrayLike(Buffer, 'big', BLOCKNUMBER_BYTE_SIZE) },
@@ -81,6 +107,7 @@ class State {
     }
     delete this.lock[recipient]
     delete this.lock[type]
+    return newTotalDeposits
   }
 }
 
---
+++ test/test-state.js
@@ -0,0 +1,93 @@
+/* eslint-env mocha */
+
+const fs = require('fs')
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const constants = require('../constants.js')
+const accounts = require('./mock-accounts.js').accounts
+const web3 = require('../eth.js')
+const BN = web3.utils.BN
+const State = require('../state.js')
+const levelup = require('levelup')
+const leveldown = require('leveldown')
+
+const expect = chai.expect
+
+chai.use(chaiHttp)
+
+describe('State', function () {
+  let db
+  let state
+  beforeEach(async () => {
+    db = levelup(leveldown('./test-db/' + +new Date()))
+    // Create a new tx-log dir for this test
+    const txLogDirectory = './test-db/' + +new Date() + '-tx-log/'
+    fs.mkdirSync(txLogDirectory)
+    // Create state object
+    state = new State.State(db, txLogDirectory)
+    await state.init()
+  })
+
+  describe('addDeposit', () => {
+    it('increments total deposits by the deposit amount', async () => {
+      const addr0 = Buffer.from(web3.utils.hexToBytes(accounts[0].address))
+      const ethType = new BN(0)
+      // const tokenType = new BN(1)
+      const depositAmount = new BN(10)
+      // There should be no total deposits record
+      try {
+        await db.get(State.getTotalDepositsKey(ethType))
+        // If this succeeded something is going wrong
+        throw new Error('Expected no entry for this type')
+      } catch (err) {
+        if (!err.notFound) throw err
+        // Otherwise we (correctly) have no total_deposits record for this type
+      }
+      // Add a deposit
+      await state.addDeposit(addr0, ethType, depositAmount)
+      let totalEthDeposits = await db.get(State.getTotalDepositsKey(ethType))
+      // Check that our total deposits equal 10
+      expect(totalEthDeposits).to.deep.equal(new BN(10).toArrayLike(Buffer, 'big', constants.TYPE_BYTE_SIZE))
+    })
+  })
+  describe('startNewBlock', () => {
+    it('increments the current blocknumber', async () => {
+      const addr0 = Buffer.from(web3.utils.hexToBytes(accounts[0].address))
+      const ethType = new BN(0)
+      // const tokenType = new BN(1)
+      const depositAmount = new BN(10)
+      // Add a deposit
+      await state.addDeposit(addr0, ethType, depositAmount)
+      let totalEthDeposits = await db.get(State.getTotalDepositsKey(ethType))
+      // Check that our total deposits equal 10
+      expect(totalEthDeposits).to.deep.equal(new BN(10).toArrayLike(Buffer, 'big', constants.TYPE_BYTE_SIZE))
+      // Increment the blocknumber
+      await state.startNewBlock()
+      expect(state.blocknumber).to.deep.equal(new BN(1))
+    })
+
+    it('should lock deposits while changing blocknumber', async () => {
+      const addr0 = Buffer.from(web3.utils.hexToBytes(accounts[0].address))
+      const ethType = new BN(0)
+      // const tokenType = new BN(1)
+      const depositAmount = new BN(10)
+      // Add a deposit
+      await state.addDeposit(addr0, ethType, depositAmount)
+      // Now add a bunch of conflicting deposits. This will trigger a bunch of locks
+      for (let i = 0; i < 20; i++) {
+        state.addDeposit(addr0, ethType, depositAmount).then((res) => {
+          console.log('Added deposit. New total deposits: ', res)
+        })
+      }
+      let totalEthDeposits = await db.get(State.getTotalDepositsKey(ethType))
+      console.log('this is our total deposits:' + new BN(totalEthDeposits))
+      // Check that our total deposits equal 10
+      expect(totalEthDeposits).to.deep.equal(new BN(10).toArrayLike(Buffer, 'big', constants.TYPE_BYTE_SIZE))
+      // Increment the blocknumber
+      await state.startNewBlock()
+      expect(state.blocknumber).to.deep.equal(new BN(1))
+      console.log('TOTAL ETH::::')
+      console.log(totalEthDeposits)
+    })
+  })
+})
