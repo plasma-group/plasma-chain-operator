2cf08acc68571539cdbb6d73310377ef6aa6e92d
Karl Floersch <karl@karlfloersch.com>
2019-01-15 12:03:55 -0500

    Fix production init in eth.js

--- src/eth.js
+++ src/eth.js
@@ -2,7 +2,6 @@ const path = require('path')
 const fs = require('fs')
 const plasmaChainCompiled = require('plasma-contracts').plasmaChainCompiled
 const plasmaRegistryCompiled = require('plasma-contracts').plasmaRegistryCompiled
-const mineBlock = require('plasma-contracts').utils.ethRPC.mineBlock
 const Web3 = require('web3')
 const ganache = require('ganache-cli')
 const log = require('debug')('info:eth')
@@ -14,6 +13,10 @@ let plasmaChain = 'UNINITIALIZED'
 let operatorAddress = 'UNINITIALIZED'
 
 async function startup (config) {
+  // TODO: Remove this and replace with proper account management
+  web3 = new Web3()
+  _addTestWalletsToWeb3(web3)
+  operatorAddress = web3.eth.accounts.wallet[0].address
   // Check if we are in test mode
   if (process.env.NODE_ENV === 'test') {
     await initializeTestingEnv(config)
@@ -21,44 +24,59 @@ async function startup (config) {
     await initializeProdEnv(config)
   }
   // Create our plasma chain web3 object, this will point to an existing Ethereum smart contract
-  const plasmaChainAddress = fs.readFileSync(path.join(config.dbDir, PLASMA_CHAIN_ADDRESS_FILENAME))
+  const plasmaChainAddress = fs.readFileSync(path.join(config.dbDir, PLASMA_CHAIN_ADDRESS_FILENAME), 'utf8')
   plasmaChain = new web3.eth.Contract(plasmaChainCompiled.abi, plasmaChainAddress)
+  console.log('Using Plasma Chain at address:', plasmaChainAddress)
 }
 
 async function initializeTestingEnv (config) {
   // First get our web3 object which we will use. This comes with some wallets that have $ in them
-  web3 = _getTestWeb3()
-  operatorAddress = web3.eth.accounts.wallet[0].address
-  // Now deploy a new PlasmaRegistry. This requires first deploying a dummy Plasma Chain
+  _setupTestProvider(web3)
+  // Deploy a new Plasma Registry
+  await deployNewPlasmaRegistry(config)
+  // Deploy our new Plasma Chain and save it in a file
+  const plasmaContractAddress = await deployNewPlasmaChain(web3, config)
+  fs.writeFileSync(path.join(config.dbDir, PLASMA_CHAIN_ADDRESS_FILENAME), plasmaContractAddress)
+}
+
+async function deployNewPlasmaRegistry (config) {
+  // Deploy a new PlasmaRegistry. This requires first deploying a dummy Plasma Chain
   // We have the compiled contracts, let's create objects for them...
   const plasmaChainCt = new web3.eth.Contract(plasmaChainCompiled.abi, operatorAddress, {from: operatorAddress, gas: 3500000, gasPrice: '300000'})
   const plasmaRegistryCt = new web3.eth.Contract(plasmaRegistryCompiled.abi, operatorAddress, {from: operatorAddress, gas: 3500000, gasPrice: '300000'})
   // To set up the Plasma Network, we need to first deploy a Plasma Chain contract
   const plasmaChain = await plasmaChainCt.deploy({ data: plasmaChainCompiled.bytecode }).send()
-  await mineBlock(web3)
+  // await mineBlock(web3)
   // Finally deploy the Plasma Registry and save the address in our config
   const plasmaRegistry = await plasmaRegistryCt.deploy({ data: plasmaRegistryCompiled.bytecode }).send()
   config.plasmaRegistryAddress = plasmaRegistry.options.address
   log('Deployed a Plasma Registry at', config.plasmaRegistryAddress)
-  await mineBlock(web3)
+  // await mineBlock(web3)
   // Initialize the registry
   await plasmaRegistry.methods.initializeRegistry(plasmaChain.options.address).send()
-  // Now deploy the new Plasma Chain and save it in a file
-  const plasmaContractAddress = await deployNewPlasmaChain(web3, config)
-  fs.writeFileSync(path.join(config.dbDir, PLASMA_CHAIN_ADDRESS_FILENAME), plasmaContractAddress)
 }
 
 async function initializeProdEnv (config) {
-  if (config.web3Provider === undefined) {
+  if (config.web3HttpProvider === undefined) {
     throw new Error('Web3 provider undefined!')
   }
-  web3 = new Web3(config.web3Provider)
+  web3.setProvider(new Web3.providers.HttpProvider(config.web3HttpProvider))
+  // Check if we need to deploy a new Plasma registry. TODO: Save the deployed registry address automatically
+  if (config.plasmaRegistryAddress === 'NONE') {
+    await deployNewPlasmaRegistry(config)
+    console.log('Deployed new registry to address:', config.plasmaRegistryAddress, '--save this in your config')
+  }
   // Check if we have a Plasma Contract already deployed
   const plasmaChainAddressPath = path.join(config.dbDir, PLASMA_CHAIN_ADDRESS_FILENAME)
   if (!fs.existsSync(plasmaChainAddressPath)) {
+    // Check that the plasma registry was deployed
+    const plasmaRegistryCode = await web3.eth.getCode(config.plasmaRegistryAddress)
+    if (plasmaRegistryCode === '0x') {
+      throw new Error('No plasma registry found at address: ' + config.plasmaRegistryAddress)
+    }
     // Deploy a new Plasma Chain and save it in a file
-    const plasmaContractAddress = deployNewPlasmaChain(web3, config)
-    console.log('No Plasma Chain detected! Creating a new one at address:', plasmaContractAddress)
+    const plasmaContractAddress = await deployNewPlasmaChain(web3, config)
+    console.log('No Plasma Chain detected! Created a new one at address:', plasmaContractAddress)
     fs.writeFileSync(path.join(config.dbDir, PLASMA_CHAIN_ADDRESS_FILENAME), plasmaContractAddress)
   }
 }
@@ -72,23 +90,25 @@ async function deployNewPlasmaChain (web3, config) {
   return newPlasmaChainAddress
 }
 
-function _getTestWeb3 () {
-  const w3 = new Web3()
-  const ganacheAccounts = []
+function _addTestWalletsToWeb3 (web3) {
   for (let i = 0; i < 5; i++) {
-    const privateKey = w3.utils.sha3(i.toString())
+    web3.eth.accounts.wallet.add(web3.utils.sha3(i.toString()))
+  }
+}
+
+function _setupTestProvider (web3) {
+  const ganacheAccounts = []
+  for (let i = 0; i < web3.eth.accounts.wallet.length; i++) {
     ganacheAccounts.push({
       balance: '0x99999999991',
-      secretKey: privateKey
+      secretKey: web3.eth.accounts.wallet[i].privateKey
     })
-    w3.eth.accounts.wallet.add(privateKey)
   }
   // For all provider options, see: https://github.com/trufflesuite/ganache-cli#library
-  const providerOptions = {'accounts': ganacheAccounts, 'locked': false, 'logger': console}
-  w3.setProvider(ganache.provider(providerOptions))
+  const providerOptions = {'accounts': ganacheAccounts, 'locked': false, 'logger': { log }}
+  web3.setProvider(ganache.provider(providerOptions))
   // TODO: Remove this as it is squashing errors. See https://github.com/ethereum/web3.js/issues/1648
-  w3.currentProvider.setMaxListeners(300)
-  return w3
+  web3.currentProvider.setMaxListeners(300)
 }
 
 module.exports = {
